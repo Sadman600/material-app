db0ea3bfa4e7b1ffa2b6b0f3068d828cc94cad80
Sadman600 <sadmansakib600@gmail.com>
2023-01-31 00:51:22 +0600

    Edited some component text

--- componants/Product.js
+++ componants/Product.js
@@ -25,7 +25,7 @@ const Product = ({ product }) => {
                                 Lizards are a widespread group of squamate reptiles, with over 6,000
                                 species, ranging across all continents except Antarctica
                             </Typography>
-                            {/* <Button>Details</Button> */}
+                            
                         </Stack>
                     </CardContent>
                 </CardActionArea>
